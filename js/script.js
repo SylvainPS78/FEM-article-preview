8f6ad15cebab86dda3ec647ea0f69dfb5b806ae4
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-17 15:58:50 +0700

    FEAT Create basic desktop CSS and js
    Need to fix menu position on Desktop

--- js/script.js
+++ js/script.js
@@ -2,13 +2,27 @@ const linkBtn = document.getElementById("link-btn");
 const linkBtn2 = document.getElementById("link-btn2");
 const authorInfo = document.getElementById("author-info");
 const shareLinks = document.getElementById("share-links");
+let shareMenuStatus = false;
 
-linkBtn.addEventListener("click",()=>{
-    authorInfo.style.display = "none";
-    shareLinks.style.display = "flex";
-})
+if (window.innerWidth < 1000) {
+    linkBtn.addEventListener("click",()=>{
+        authorInfo.style.display = "none";
+        shareLinks.style.display = "flex";
+    })
 
-linkBtn2.addEventListener("click",()=>{
-    authorInfo.style.display = "grid";
-    shareLinks.style.display = "none";
-})
+    linkBtn2.addEventListener("click",()=>{
+        authorInfo.style.display = "grid";
+        shareLinks.style.display = "none";
+    })
+}
+else {
+    linkBtn.addEventListener("click", ()=> {
+        if (!shareMenuStatus) {
+            shareLinks.style.display = "flex";
+            shareMenuStatus = true;
+        }
+        else {shareLinks.style.display = "none";
+            shareMenuStatus = false;
+        }
+    })
+}
